1647eaece7df62ed3eb3d68030d494824a0fb72c
Vion560 <113586244+Vion560@users.noreply.github.com>
2022-09-29 09:36:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace depurar_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a;
+            float b;
+            float multiplicar;
+            Console.WriteLine("Vamos a multiplicar");
+            a = 11;
+            b = 7;
+            multiplicar = (a * b);
+        }
+    }
+}
